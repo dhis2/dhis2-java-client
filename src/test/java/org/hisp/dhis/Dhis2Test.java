14282f14fd515b745deb7ca10698744e8f001cde
Lars Helge Øverland <lars@dhis2.org>
2018-04-12 12:36:40 +0200

    Unit test

---
+++ src/test/java/org/hisp/dhis/Dhis2Test.java
@@ -0,0 +1,21 @@
+package org.hisp.dhis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class Dhis2Test
+{
+    @Test
+    public void testDhis2()
+    {
+        Dhis2Config dhis2Config = new Dhis2Config( 
+            "https://play.dhis2.org/2.29/api", 
+            "admin", 
+            "district" );
+        
+        Dhis2 dhis2 = new Dhis2( dhis2Config );
+        
+        assertNotNull( dhis2 );
+    }
+}
