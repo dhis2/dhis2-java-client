6c4cac332fec18976889707a2fc6326d1a906ac0
Lars Helge Øverland <lars@dhis2.org>
2020-09-25 16:57:59 +0200

    Add SystemSettings object

---
+++ src/main/java/org/hisp/dhis/model/SystemSettings.java
@@ -0,0 +1,34 @@
+package org.hisp.dhis.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SystemSettings
+{
+    @JsonProperty(value="keyCalendar")
+    private String calendar;
+
+    @JsonProperty(value="keyDateFormat")
+    private String dateFormat;
+
+    @JsonProperty(value="keyDbLocale")
+    private String dbLocale;
+
+    @JsonProperty(value="keyDbLocale")
+    private String uiLocale;
+
+    @JsonProperty(value="keyLastSuccessfulAnalyticsTablesUpdate")
+    private Date lastSuccessfulAnalyticsTablesUpdate;
+
+    @JsonProperty(value="keyLastSuccessfulResourceTablesUpdate")
+    private Date lastSuccessfulResourceTablesUpdate;
+
+    @JsonProperty(value="lastSuccessfulSystemMonitoringPush")
+    private Date keyLastSuccessfulSystemMonitoringPush;
+}
