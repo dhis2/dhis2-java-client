9a5a97d77b18c0e802d161d0e31fc21cd66c11e9
Lars Helge Øverland <lars@dhis2.org>
2021-03-15 14:57:52 +0100

    feat: Add tracked entity attribute support for programs (#33)

--- src/test/java/org/hisp/dhis/Dhis2ApiTest.java
+++ src/test/java/org/hisp/dhis/Dhis2ApiTest.java
@@ -4,12 +4,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.io.UncheckedIOException;
 import java.util.List;
 
 import org.hisp.dhis.category.IntegrationTest;
+import org.hisp.dhis.model.DataElement;
 import org.hisp.dhis.model.OrgUnitGroup;
+import org.hisp.dhis.model.Program;
+import org.hisp.dhis.model.ProgramStage;
+import org.hisp.dhis.model.ProgramStageDataElement;
+import org.hisp.dhis.model.ProgramType;
+import org.hisp.dhis.model.TrackedEntityAttribute;
+import org.hisp.dhis.query.Filter;
 import org.hisp.dhis.query.Query;
 import org.hisp.dhis.response.Dhis2ClientException;
 import org.junit.Test;
@@ -45,6 +53,117 @@ public class Dhis2ApiTest
     }
 
     @Test
+    public void testGetProgram()
+    {
+        Dhis2 dhis2 = new Dhis2( config );
+
+        Program program = dhis2.getProgram( "IpHINAT79UW" );
+
+        assertNotNull( program );
+        assertEquals( "IpHINAT79UW", program.getId() );
+        assertEquals( ProgramType.WITH_REGISTRATION, program.getProgramType() );
+
+        assertFalse( program.getTrackedEntityAttributes().isEmpty() );
+        assertNotNull( program.getTrackedEntityAttributes().get( 0 ) );
+
+        assertFalse( program.getProgramTrackedEntityAttributes().isEmpty() );
+        assertNotNull( program.getProgramTrackedEntityAttributes().get( 0 ) );
+        assertNotNull( program.getProgramTrackedEntityAttributes().get( 0 ).getId() );
+
+        TrackedEntityAttribute tea = program.getProgramTrackedEntityAttributes().get( 0 ).getTrackedEntityAttribute();
+
+        assertNotNull( tea );
+        assertNotNull( tea.getId() );
+        assertNotNull( tea.getValueType() );
+
+        assertFalse( program.getProgramStages().isEmpty() );
+
+        ProgramStage ps = program.getProgramStages().get( 0 );
+
+        assertNotNull( ps );
+        assertNotNull( ps.getId() );
+        assertFalse( ps.getProgramStageDataElements().isEmpty() );
+
+        ProgramStageDataElement psde = ps.getProgramStageDataElements().get( 0 );
+
+        assertNotNull( psde );
+        assertNotNull( psde.getId() );
+
+        DataElement de = psde.getDataElement();
+
+        assertNotNull( de );
+        assertNotNull( de.getId() );
+        assertNotNull( de.getAggregationType() );
+        assertNotNull( de.getValueType() );
+    }
+
+    @Test
+    public void testGetPrograms()
+    {
+        Dhis2 dhis2 = new Dhis2( config );
+
+        List<Program> programs = dhis2.getPrograms( Query.instance() );
+
+        assertNotNull( programs );
+        assertFalse( programs.isEmpty() );
+        assertNotNull( programs.get( 0 ) );
+    }
+
+    @Test
+    public void testGetProgramsExpandAssociations()
+    {
+        Dhis2 dhis2 = new Dhis2( config );
+
+        List<Program> programs = dhis2.getPrograms( Query.instance()
+            .withExpandAssociations()
+            .addFilter( Filter.eq( "id", "IpHINAT79UW" ) ) );
+
+        assertNotNull( programs );
+        assertEquals( 1, programs.size() );
+        assertNotNull( programs.get( 0 ) );
+        assertEquals( "IpHINAT79UW", programs.get( 0 ).getId() );
+
+        Program program = programs.get( 0 );
+
+        assertNotNull( program );
+        assertEquals( "IpHINAT79UW", program.getId() );
+        assertEquals( ProgramType.WITH_REGISTRATION, program.getProgramType() );
+
+        assertFalse( program.getTrackedEntityAttributes().isEmpty() );
+        assertNotNull( program.getTrackedEntityAttributes().get( 0 ) );
+
+        assertFalse( program.getProgramTrackedEntityAttributes().isEmpty() );
+        assertNotNull( program.getProgramTrackedEntityAttributes().get( 0 ) );
+        assertNotNull( program.getProgramTrackedEntityAttributes().get( 0 ).getId() );
+
+        TrackedEntityAttribute tea = program.getProgramTrackedEntityAttributes().get( 0 ).getTrackedEntityAttribute();
+
+        assertNotNull( tea );
+        assertNotNull( tea.getId() );
+        assertNotNull( tea.getValueType() );
+
+        assertFalse( program.getProgramStages().isEmpty() );
+
+        ProgramStage ps = program.getProgramStages().get( 0 );
+
+        assertNotNull( ps );
+        assertNotNull( ps.getId() );
+        assertFalse( ps.getProgramStageDataElements().isEmpty() );
+
+        ProgramStageDataElement psde = ps.getProgramStageDataElements().get( 0 );
+
+        assertNotNull( psde );
+        assertNotNull( psde.getId() );
+
+        DataElement de = psde.getDataElement();
+
+        assertNotNull( de );
+        assertNotNull( de.getId() );
+        assertNotNull( de.getAggregationType() );
+        assertNotNull( de.getValueType() );
+    }
+
+    @Test
     public void testGetInvalidUrl()
     {
         Dhis2 dhis2 = new Dhis2( new Dhis2Config( "https://not_a_domain.abc", "username", "pw" ) );
@@ -62,7 +181,6 @@ public class Dhis2ApiTest
             () -> dhis2.getOrgUnitGroups( Query.instance() ) );
 
         assertEquals( 401, ex.getStatusCode() );
-        assertEquals( "Access denied", ex.getMessage() );
     }
 
     @Test
@@ -74,6 +192,5 @@ public class Dhis2ApiTest
             () -> dhis2.getOrgUnitGroup( "NonExisting" ) );
 
         assertEquals( 404, ex.getStatusCode() );
-        assertEquals( "Object not found", ex.getMessage() );
     }
 }
