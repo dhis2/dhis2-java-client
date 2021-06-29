cb1e6185a022983c3818218b9b49e09783b2a993
Lars Helge Øverland <lars@dhis2.org>
2021-06-29 21:43:18 +0200

    Update test

--- src/test/java/org/hisp/dhis/Dhis2ApiTest.java
+++ src/test/java/org/hisp/dhis/Dhis2ApiTest.java
@@ -27,7 +27,7 @@ import org.junit.experimental.categories.Category;
 public class Dhis2ApiTest
 {
     private static final Dhis2Config config = new Dhis2Config(
-        "https://play.dhis2.org/2.35.4", "admin", "district" );
+        "https://play.dhis2.org/2.35.5", "admin", "district" );
 
     @Test
     public void testGetOrgUnits()
