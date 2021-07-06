8015e0fd322fc79d95c1510f0a936a40ecf74ae2
Lars Helge Øverland <lars@dhis2.org>
2021-07-06 08:34:05 +0200

    Set chain true accessor for data value set

--- src/main/java/org/hisp/dhis/model/datavalueset/DataValueSet.java
+++ src/main/java/org/hisp/dhis/model/datavalueset/DataValueSet.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Getter
 @Setter
+@Accessors(chain=true)
 public class DataValueSet
 {
     @JsonProperty
