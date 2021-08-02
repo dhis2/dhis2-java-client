6e1cecabd5f39572cbdecb976a78854cf8dee71f
Lars Helge Øverland <lars@dhis2.org>
2021-08-02 11:33:54 +0200

    Add data store test

--- src/test/java/org/hisp/dhis/DataStoreApiTest.java
+++ src/test/java/org/hisp/dhis/DataStoreApiTest.java
@@ -1,33 +1,57 @@
 package org.hisp.dhis;
 
-import lombok.AllArgsConstructor;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.hisp.dhis.category.IntegrationTest;
+import org.hisp.dhis.response.Response;
+import org.hisp.dhis.response.Status;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Category( IntegrationTest.class )
 public class DataStoreApiTest
 {
     @Test
-    public void testSaveGet()
+    public void testCrud()
     {
         Dhis2 dhis2 = new Dhis2( TestFixture.DEFAULT_CONFIG );
 
         Fruit banana = new Fruit( "Banana", "Yellow" );
         Fruit grape = new Fruit( "Grape", "Green" );
 
-    }
+        Response rA = dhis2.saveDataStoreEntry( "fruits", "banana", banana );
+        Response rB = dhis2.saveDataStoreEntry( "fruits", "grape", grape );
 
-    @AllArgsConstructor
-    class Fruit
-    {
-        @JsonProperty
-        private String name;
+        assertEquals( Status.OK, rA.getStatus() );
+        assertEquals( Status.OK, rB.getStatus() );
 
-        @JsonProperty
-        private String color;
+        banana = dhis2.getDataStoreEntry( "fruits", "banana", Fruit.class );
+        grape = dhis2.getDataStoreEntry( "fruits", "grape", Fruit.class );
+
+        assertNotNull( banana );
+        assertEquals( "Banana", banana.getName() );
+        assertEquals( "Yellow", banana.getColor() );
+
+        assertNotNull( grape );
+        assertEquals( "Grape", grape.getName() );
+        assertEquals( "Green", grape.getColor() );
+
+        Fruit redBanana = new Fruit( "Banana", "Red" );
+
+        Response rC = dhis2.updateDataStoreEntry( "fruits", "banana", redBanana );
+
+        assertEquals( Status.OK, rC.getStatus() );
+
+        redBanana = dhis2.getDataStoreEntry( "fruits", "banana", Fruit.class );
+
+        assertNotNull( redBanana );
+        assertEquals( "Red", redBanana.getColor() );
+
+        Response rD = dhis2.removeDataStoreEntry( "fruits", "banana" );
+        Response rE = dhis2.removeDataStoreEntry( "fruits", "grape" );
+
+        assertEquals( Status.OK, rD.getStatus() );
+        assertEquals( Status.OK, rE.getStatus() );
     }
 }
