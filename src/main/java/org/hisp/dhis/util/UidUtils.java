795ed0a55746b7e849a7b245da5c593713b3d5d8
Lars Helge Øverland <lars@dhis2.org>
2021-09-07 23:16:31 +0200

    feat: Add isValidUid method (#50)

--- src/main/java/org/hisp/dhis/util/UidUtils.java
+++ src/main/java/org/hisp/dhis/util/UidUtils.java
@@ -1,6 +1,7 @@
 package org.hisp.dhis.util;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Pattern;
 
 public class UidUtils
 {
@@ -10,6 +11,8 @@ public class UidUtils
     private static final String ALLOWED_CHARS = "0123456789" + letters;
 
     private static final int NUMBER_OF_CODEPOINTS = ALLOWED_CHARS.length();
+
+    private static final Pattern UID_PATTERN = Pattern.compile( "^[a-zA-Z]{1}[a-zA-Z0-9]{10}$" );
 
     private static final int CODESIZE = 11;
 
@@ -27,6 +30,17 @@ public class UidUtils
     public static String generateUid()
     {
         return generateCode( CODESIZE );
+    }
+
+    /**
+     * Tests whether the given code is a valid UID.
+     *
+     * @param code the code to validate.
+     * @return true if the code is valid.
+     */
+    public static boolean isValidUid( String code )
+    {
+        return code != null && UID_PATTERN.matcher( code ).matches();
     }
 
     /**
