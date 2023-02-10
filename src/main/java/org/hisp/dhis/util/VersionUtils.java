48dea9d75264d1b9f97fec085d27308e750b350f
Lars Helge Øverland <lars@dhis2.org>
2023-02-10 10:25:17 +0100

    feat: Add VersionUtils

---
+++ src/main/java/org/hisp/dhis/util/VersionUtils.java
@@ -0,0 +1,54 @@
+package org.hisp.dhis.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class VersionUtils
+{
+    private static final Pattern PATTERN_MAJOR_VERSION = Pattern.compile( "\\d\\.(\\d{2}).*$" );
+
+    private static final Pattern PATTERN_PATCH_VERSION = Pattern.compile( "\\d\\.\\d{2}\\.(\\d+).*$" );
+
+    /**
+     * Returns the major version as an integer based on the given version
+     * string.
+     *
+     * @param version the version string.
+     * @return the major version.
+     */
+    public static Integer getMajorVersion( String version )
+    {
+        return asInt( getFirstGroup( PATTERN_MAJOR_VERSION, orEmpty( version ) ) );
+    }
+
+    /**
+     * Returns the patch version as an integer based on the given version
+     * string.
+     *
+     * @param version the version string.
+     * @return the patch version.
+     */
+    public static Integer getPatchVersion( String version )
+    {
+        return asInt( getFirstGroup( PATTERN_PATCH_VERSION, orEmpty( version ) ) );
+    }
+
+    private static Integer asInt( String string )
+    {
+        return string != null ? Integer.valueOf( string ) : null;
+    }
+
+    private static String getFirstGroup( Pattern pattern, String string )
+    {
+        Matcher matcher = pattern.matcher( string );
+        return matcher.matches() ? matcher.group( 1 ) : null;
+    }
+
+    private static String orEmpty( String string )
+    {
+        return ObjectUtils.firstNonNull( string, StringUtils.EMPTY );
+    }
+}
---
+++ src/test/java/org/hisp/dhis/util/VersionUtilsTest.java
@@ -0,0 +1,35 @@
+package org.hisp.dhis.util;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class VersionUtilsTest
+{
+    @Test
+    void testGetMajorVersion()
+    {
+        assertEquals( 37, VersionUtils.getMajorVersion( "2.37.8.1-SNAPSHOT" ) );
+        assertEquals( 28, VersionUtils.getMajorVersion( "2.28" ) );
+        assertEquals( 35, VersionUtils.getMajorVersion( "2.35.11" ) );
+        assertEquals( 37, VersionUtils.getMajorVersion( "2.37.9" ) );
+        assertEquals( 35, VersionUtils.getMajorVersion( "2.35.8.1-SNAPSHOT" ) );
+        assertEquals( 38, VersionUtils.getMajorVersion( "2.38.1-EMBARGOED" ) );
+        assertNull( VersionUtils.getMajorVersion( "FOO" ) );
+        assertNull( VersionUtils.getMajorVersion( null ) );
+    }
+
+    @Test
+    void testGetPatchVersion()
+    {
+        assertEquals( 8, VersionUtils.getPatchVersion( "2.37.8.1-SNAPSHOT" ) );
+        assertNull( VersionUtils.getPatchVersion( "2.28" ) );
+        assertEquals( 11, VersionUtils.getPatchVersion( "2.35.11" ) );
+        assertEquals( 9, VersionUtils.getPatchVersion( "2.37.9" ) );
+        assertEquals( 8, VersionUtils.getPatchVersion( "2.35.8.1-SNAPSHOT" ) );
+        assertEquals( 1, VersionUtils.getPatchVersion( "2.38.1-EMBARGOED" ) );
+        assertNull( VersionUtils.getPatchVersion( "FOO" ) );
+        assertNull( VersionUtils.getMajorVersion( null ) );
+    }
+}
