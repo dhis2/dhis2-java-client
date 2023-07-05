a8f7081548f770a701f0d0f0f59aa9def3bdf70e
Olaniyan Folajimi <jimiolaniyan@gmail.com>
2023-07-05 15:02:31 +0100

    feat: add category option groups and update models

--- src/main/java/org/hisp/dhis/model/CategoryOptionGroup.java
+++ src/main/java/org/hisp/dhis/model/CategoryOptionGroup.java
@@ -4,26 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class DataElementGroupSet
+public class CategoryOptionGroup
     extends NameableObject
 {
-    @JsonProperty
-    private Boolean compulsory;
-
-    @JsonProperty
-    private Boolean dataDimension;
-
     @JsonProperty
     private DataDimensionType dataDimensionType;
 
     @JsonProperty
-    private List<DataElementGroup> dataElementGroups = new ArrayList<>();
+    private DimensionItemType dimensionItemType;
+
+    @JsonProperty
+    private List<CategoryOption> categoryOptions = new ArrayList<>();
+
+    @JsonProperty
+    private List<CategoryOptionGroupSet> groupSets = new ArrayList<>();
 }
--- src/main/java/org/hisp/dhis/model/CategoryOptionGroupSet.java
+++ src/main/java/org/hisp/dhis/model/CategoryOptionGroupSet.java
@@ -1,6 +1,24 @@
 package org.hisp.dhis.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Getter
+@Setter
 public class CategoryOptionGroupSet
     extends NameableObject
 {
+    @JsonProperty
+    private Boolean dataDimension;
+
+    @JsonProperty
+    private DataDimensionType dataDimensionType;
+
+    @JsonProperty
+    private List<CategoryOptionGroup> categoryOptionGroups = new ArrayList<>();
 }
