09d2e605b42b2a105740c53a676695b92f760324
Olaniyan Folajimi <jimiolaniyan@gmail.com>
2023-11-21 19:43:28 +0100

    feat: get me info

---
+++ src/main/java/org/hisp/dhis/model/Me.java
@@ -0,0 +1,39 @@
+package org.hisp.dhis.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Getter
+@Setter
+public class Me
+    extends IdentifiableObject
+{
+    @JsonProperty
+    private String username;
+
+    @JsonProperty
+    private String surname;
+
+    @JsonProperty
+    private String firstName;
+
+    @JsonProperty
+    private String email;
+
+    @JsonProperty
+    private Set<String> programs = new HashSet<>();
+
+    @JsonProperty
+    private Set<String> dataSets = new HashSet<>();
+
+    @JsonProperty
+    private Set<String> authorities = new HashSet<>();
+
+    @JsonProperty
+    private Set<OrgUnit> organisationUnits = new HashSet<>();
+}
