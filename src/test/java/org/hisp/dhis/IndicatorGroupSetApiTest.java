a0271fde20a6bf5cd1b05cb54f871b7d2442ef06
Lars Helge Overland <lars@dhis2.org>
2024-03-13 13:01:51 +0000

    fix: Update code

---
+++ src/test/java/org/hisp/dhis/IndicatorGroupSetApiTest.java
@@ -0,0 +1,41 @@
+package org.hisp.dhis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.hisp.dhis.model.IndicatorGroupSet;
+import org.hisp.dhis.query.Query;
+import org.hisp.dhis.support.TestTags;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag( TestTags.INTEGRATION )
+class IndicatorGroupSetApiTest
+{
+    @Test
+    void getIndicatorGroupSet()
+    {
+        Dhis2 dhis2 = new Dhis2( TestFixture.DEFAULT_CONFIG );
+
+        IndicatorGroupSet groupSet = dhis2.getIndicatorGroupSet( "tOwnTs7TL3Y" );
+
+        assertNotNull( groupSet );
+        assertEquals( "tOwnTs7TL3Y", groupSet.getId() );
+        assertFalse( groupSet.getIndicatorGroups().isEmpty() );
+    }
+
+    @Test
+    void testGetIndicatorGroupSets()
+    {
+        Dhis2 dhis2 = new Dhis2( TestFixture.DEFAULT_CONFIG );
+
+        List<IndicatorGroupSet> groupSets = dhis2.getIndicatorGroupSets( Query.instance() );
+
+        assertNotNull( groupSets );
+        assertFalse( groupSets.isEmpty() );
+        assertNotNull( groupSets.get( 0 ).getId() );
+    }
+}
