c1360513554aaec8a50b686edfb7926d56cfbd13
Lars Helge Øverland <lars@dhis2.org>
2024-07-01 18:17:43 +0200

    fix: Bump version

--- src/test/java/org/hisp/dhis/DataSetApiTest.java
+++ src/test/java/org/hisp/dhis/DataSetApiTest.java
@@ -34,6 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
 import java.util.List;
 import org.hisp.dhis.model.DataSet;
 import org.hisp.dhis.model.DataSetElement;
