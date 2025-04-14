40894a56be82c2e76da8b369aa3bd33b475b33fd
Lars Helge Øverland <lars@dhis2.org>
2025-04-14 10:40:32 +0200

    chore: Format code (#113)

--- src/main/java/org/hisp/dhis/model/event/Event.java
+++ src/main/java/org/hisp/dhis/model/event/Event.java
@@ -27,12 +27,12 @@
  */
 package org.hisp.dhis.model.event;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
