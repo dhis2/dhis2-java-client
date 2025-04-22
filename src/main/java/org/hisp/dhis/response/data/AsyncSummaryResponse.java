95a0e9679da3e4eaf759c17880241afefb82ce71
Lars Helge Øverland <lars@dhis2.org>
2025-04-22 21:29:51 +0200

    fix: Update code

--- src/main/java/org/hisp/dhis/response/data/AsyncSummaryResponse.java
+++ src/main/java/org/hisp/dhis/response/data/AsyncSummaryResponse.java
@@ -27,16 +27,19 @@
  */
 package org.hisp.dhis.response.data;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.hisp.dhis.response.BaseHttpResponse;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.hisp.dhis.response.BaseHttpResponse;
 
+/**
+ * Asynchronous import summary response.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
