7c1c13b2420150c41ad77581a3c7ba5bd7e10252
Lars Helge Øverland <lars@dhis2.org>
2025-04-22 15:44:21 +0200

    fix: Update code

--- src/main/java/org/hisp/dhis/util/GeoUtils.java
+++ src/main/java/org/hisp/dhis/util/GeoUtils.java
@@ -45,9 +45,6 @@ public class GeoUtils {
    * @return a {@link Point}
    */
   public static Point toPoint(double longitude, double latitude) {
-
-    Coordinate coordinate = new Coordinate(10.752, 59.914);
-
-    return GEO_FACTORY.createPoint(coordinate);
+    return GEO_FACTORY.createPoint(new Coordinate(10.752, 59.914));
   }
 }
