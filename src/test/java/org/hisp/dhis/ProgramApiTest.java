c6f6d64e4cb517a87d18c067f6730c98dd955540
Lars Helge Øverland <lars@dhis2.org>
2025-05-08 15:41:42 +0200

    fix: Update code

--- src/test/java/org/hisp/dhis/ProgramApiTest.java
+++ src/test/java/org/hisp/dhis/ProgramApiTest.java
@@ -33,6 +33,7 @@ import static org.hisp.dhis.support.Assertions.assertSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.List;
 import org.hisp.dhis.model.DataElement;
 import org.hisp.dhis.model.Program;
@@ -144,7 +145,7 @@ public class ProgramApiTest {
     assertNotBlank(pr.getShortName());
     assertNotNull(pr.getProgramStages());
     assertNotNull(pr.getProgramStageSections());
-    assertFalse (pr.getProgramStageSections().isEmpty());
+    assertFalse(pr.getProgramStageSections().isEmpty());
 
     ProgramStage ps = pr.getProgramStages().get(0);
 
