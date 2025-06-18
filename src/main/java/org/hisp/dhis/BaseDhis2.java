6223a39bc0df18268b55eead8d9e5219637f5908
Lars Helge Øverland <lars@dhis2.org>
2025-06-18 12:08:18 +0200

    fix: Update code

--- src/main/java/org/hisp/dhis/BaseDhis2.java
+++ src/main/java/org/hisp/dhis/BaseDhis2.java
@@ -36,8 +36,6 @@ import static org.apache.hc.core5.http.HttpStatus.SC_UNAUTHORIZED;
 import static org.hisp.dhis.util.CollectionUtils.asList;
 import static org.hisp.dhis.util.CollectionUtils.toCommaSeparated;
 import static org.hisp.dhis.util.HttpUtils.getUriAsString;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -52,7 +50,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.hc.client5.http.HttpResponseException;
@@ -113,6 +110,8 @@ import org.hisp.dhis.response.objects.ObjectsResponse;
 import org.hisp.dhis.util.DateTimeUtils;
 import org.hisp.dhis.util.HttpUtils;
 import org.hisp.dhis.util.JacksonUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Lars Helge Overland
@@ -384,7 +383,6 @@ public class BaseDhis2 {
    */
   protected <T> T getObject(URIBuilder uriBuilder, BaseQuery query, Class<T> type) {
     URI url = getObjectQuery(uriBuilder, query);
-
     return getObjectFromUrl(url, type);
   }
 
