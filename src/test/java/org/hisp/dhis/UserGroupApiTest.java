8d04dca4fae0fa867fa06a027206ce9ddcc40165
Leonardo Gonzales <lgonzales@baosystems.com>
2025-06-23 17:13:52 +0100

    chore: Add UserGroupApiTest improve static import.

--- src/test/java/org/hisp/dhis/UserGroupApiTest.java
+++ src/test/java/org/hisp/dhis/UserGroupApiTest.java
@@ -27,7 +27,9 @@
  */
 package org.hisp.dhis;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 import org.hisp.dhis.model.user.User;
