5ae1a4bc97c2d16078e1d7644899772f47207a23
Leonardo Gonzales <lgonzales@baosystems.com>
2025-07-04 17:10:01 +0100

    chore: add Response.isStatusOk method.

--- src/main/java/org/hisp/dhis/response/Response.java
+++ src/main/java/org/hisp/dhis/response/Response.java
@@ -27,6 +27,7 @@
  */
 package org.hisp.dhis.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -138,5 +139,10 @@ public class Response extends BaseHttpResponse {
     }
 
     return null;
+  }
+
+  @JsonIgnore
+  public boolean isStatusOk() {
+    return Status.OK.equals(status);
   }
 }
