b26db92aaa50300aefb11bbf60612ed318856b64
Lars Helge Øverland <lars@dhis2.org>
2025-07-06 14:15:53 +0200

    fix: Update code

---
+++ src/main/java/org/hisp/dhis/model/MetadataObject.java
@@ -0,0 +1,10 @@
+package org.hisp.dhis.model;
+
+public interface MetadataObject {
+  /**
+   * Returns the {@link MetadataEntity}.
+   * 
+   * @return the {@link MetadataEntity}.
+   */
+  MetadataEntity getEntity();
+}
