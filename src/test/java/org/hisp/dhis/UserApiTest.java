a3719d29e198b09c253bd6f22d6ca011e9eff3bd
Leonardo Gonzales <lgonzales@baosystems.com>
2025-07-30 14:29:35 +0100

    chore: fix static import for UserApiTest.

--- src/test/java/org/hisp/dhis/UserApiTest.java
+++ src/test/java/org/hisp/dhis/UserApiTest.java
@@ -28,7 +28,10 @@
 package org.hisp.dhis;
 
 import static org.hisp.dhis.support.Assertions.assertSuccessResponse;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Set;
