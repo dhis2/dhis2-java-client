566d0082203614030c5be26134e3f2f7f17b8a22
Lars Helge Øverland <lars@dhis2.org>
2025-10-27 11:59:58 +0100

    fix: Update code

--- src/main/java/org/hisp/dhis/model/analytics/AnalyticsData.java
+++ src/main/java/org/hisp/dhis/model/analytics/AnalyticsData.java
@@ -139,7 +139,7 @@ public class AnalyticsData {
     return headers.stream()
         .filter(AnalyticsHeader::isMeta)
         .map(headers::indexOf)
-        .collect(Collectors.toSet());
+        .collect(Collectors.toUnmodifiableSet());
   }
 
   /**
