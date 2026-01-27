733abaccce44fde3ee48eeaea28beb4c6bcc096c
Lars Helge Øverland <lars@dhis2.org>
2026-01-27 16:26:46 +0100

    fix: Update code

--- src/test/java/org/hisp/dhis/BaseDhis2Test.java
+++ src/test/java/org/hisp/dhis/BaseDhis2Test.java
@@ -139,14 +139,14 @@ class BaseDhis2Test {
     String expected =
         String.format(
             """
-        %s/api/analytics\
-        ?dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU;Jtf34kNZhzP\
-        &dimension=pe:202501;202502;202503\
-        &filter=ou:ImspTQPwCqd\
-        &skipMeta=false\
-        &skipData=false\
-        &includeNumDen=true\
-        &includeMetadataDetails=true""",
+            %s/api/analytics\
+            ?dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU;Jtf34kNZhzP\
+            &dimension=pe:202501;202502;202503\
+            &filter=ou:ImspTQPwCqd\
+            &skipMeta=false\
+            &skipData=false\
+            &includeNumDen=true\
+            &includeMetadataDetails=true""",
             TestFixture.DEFAULT_URL);
 
     assertEquals(expected, decodedUrl);
